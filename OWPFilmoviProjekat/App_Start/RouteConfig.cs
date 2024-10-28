473a55acdb3f12e01d2367260da4cfae1fe501b0
MasaSaranovic <masasaranovic@gmail.com>
2024-10-28 13:40:16 +0100

    Vezbe 3

--- OWPFilmoviProjekat/App_Start/RouteConfig.cs
+++ OWPFilmoviProjekat/App_Start/RouteConfig.cs
@@ -13,6 +13,8 @@ namespace OWPFilmoviProjekat
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapMvcAttributeRoutes();
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",

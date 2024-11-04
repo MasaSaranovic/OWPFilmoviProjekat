d25237121220c86ff6bdcaa1681686829da7466e
MasaSaranovic <masasaranovic@gmail.com>
2024-11-04 13:45:36 +0100

    Vezbe 4

--- OWPFilmoviProjekat/Controllers/MoviesController.cs
+++ OWPFilmoviProjekat/Controllers/MoviesController.cs
@@ -1,4 +1,5 @@
 ﻿using OWPFilmoviProjekat.Models;
+using OWPFilmoviProjekat.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,12 +10,39 @@ namespace OWPFilmoviProjekat.Controllers
 {
     public class MoviesController : Controller
     {
+
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek"},
+                new Movie { Id = 2, Name = "Wall-e"}
+            };
+        }
         // GET: Movies
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
 
-            return View(movie);
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer 1"},
+                new Customer { Name = "Customer 2"}
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
 
             //return Content("Hello world");
             //return HttpNotFound();
@@ -28,21 +56,7 @@ namespace OWPFilmoviProjekat.Controllers
             return Content("id = " + id);
         }
 
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue)
-            {
-                pageIndex = 1;
-            }
-
-            if (String.IsNullOrWhiteSpace(sortBy))
-            {
-                sortBy = "Name";
-            }
-
-            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
-        }
-
+        
         [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
         public ActionResult ByReleaseDate(int year, int month)
         {

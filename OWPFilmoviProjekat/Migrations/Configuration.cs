391e6620a54922c40dce095c883688f473e4a531
MasaSaranovic <masasaranovic@gmail.com>
2024-11-18 13:46:59 +0100

    Vezbe 6

---
+++ OWPFilmoviProjekat/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace OWPFilmoviProjekat.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<OWPFilmoviProjekat.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(OWPFilmoviProjekat.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
--- OWPFilmoviProjekat/Models/Customer.cs
+++ OWPFilmoviProjekat/Models/Customer.cs
@@ -9,5 +9,10 @@ namespace OWPFilmoviProjekat.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        public bool isSubscribedToNewsletter { get; set; }
+
+        public MembershipType MembershipType { get; set; }
+        public byte MembershipTypeId { get; set; }
     }
 }
